23314abfa5c08e1c16ffb5cefe7c5dac969416b0
Paula_Estay <paulaestayc@gmail.com>
2019-05-02 08:33:06 -0400

    Filtrado de data según tipo

--- src/data.js
+++ src/data.js
@@ -1,12 +1,45 @@
 /* Manejo de data */
 //filtrar, ordenar y calculo
-window.data = {
-  filterPokeType: (arr,type) => {
-    const pokeType = arr.filter(element=>{
-      return element.type.includes(type);
-    })
-    return pokeType;
-  }
 
 
+
+const filterPokeType = (data, condition)=>{
+  const resultFilter = data.filter(element =>{
+    return element.type.includes(condition);
+  }) 
+  return resultFilter;
 }
+
+window.filterPokeType=filterPokeType;
+
+
+// window.data = {
+//   filterPokeType: (data,condition) => {
+//     const pokemonByType = arr.filter(element=>{
+//       return element.type.includes("condition");
+//     })
+//     return pokemonByType;
+//   }
+
+/*filterPokeType(arrayPokemon, chosenType);
+
+
+// window.data = {
+//   filterPokeType: (arr,condition) => {
+//     const pokemonByType = arr.filter(element=>{
+//       return element.type.includes(condition);
+//     })
+//     return pokemonByType;
+//   }
+
+// filterPokeType(arrayPokemon, chosenType);
+//}
+
+//  const filterPokeType = (arr, condition) => {
+//     const pokemonByType = arr.filter(element=>{
+//       return element.type.includes(condition);
+//     });
+//     return pokemonByType;
+//   }
+
+//   window.filterPokeType = filterPokeType */
--- src/main.js
+++ src/main.js
@@ -1,43 +1,101 @@
-
-/* Manejo de data */
+/* Manejo de DOM */
 // puedes ver como agregamos la función a nuestro objeto global window
 //DECLARAMOS NUESTROS ARRAY DEL JSON
-let arrayPokemon = POKEMON.pokemon;
+let dataPokemon = window.POKEMON.pokemon;
 //CONTENEDOR DONDE MOSTRAREMOS LA INFO
-const mostrarTodos = document.getElementById("root");
-const crearPlantilla = (data) => {
+const showAll = document.getElementById("root");
+const createCards = (data) => {
+let pokemonCard ='';
   //RECORREMOS EL ARRAY 
-  data.forEach((personaje) => {
+  data.forEach((pokemon) => {
     //CREAMOS LA TARJETA CON LOS ELEMENTOS DEL ARRAY
-    mostrarTodos.innerHTML += `
-            <div class="col-3">
-            <div class="card" style="width: 18rem;">
-            <div class="card-body">
-            <img src="${ personaje.img }" /><br>
-            <b>Nº</b>${ personaje.num }</b><br>
-            <b>${ personaje.name }</b><br>
-            <b>Tipo </b><br> ${ personaje.weaknesses }</b><br>
-            <b>Debilidades </b><br> ${ personaje.weaknesses }<br>
-            <b>Huevos </b><br> ${personaje.egg }</b><br><br>
-            </div>
-            </div>
-            </div>
-            
+    card = `
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <img src="${ pokemon.img }" /><br>
+    <b>Nº </b>${ pokemon.num }</b><br>
+    <b>${ pokemon.name }</b><br>
+    <b>Tipo </b><br> ${ pokemon.weaknesses }</b><br>
+    <b>Debilidades </b><br> ${ pokemon.weaknesses }<br>
+    <b>Huevos </b><br> ${pokemon.egg }</b><br><br>
+    </div>
+    </div>S
+    </div>
     `;
+    pokemonCard += card
   })
+  showAll.innerHTML=pokemonCard
 }
 //MOSTRAMOS LA DATA DESDE EL JSON
-crearPlantilla(arrayPokemon)
+createCards(dataPokemon)
 
 
-const pokemonTypeMenu = document.querySelector(".pokemonTypes");
+// OBTENIENDO VALOR DE TIPO SELECCIONADO POR USUARIO EN DROPDOWN
+const selectType = document.getElementById("filterType");
+const containerType = document.getElementById("root");
 
-pokemonTypeMenu.addEventListener("change", (event) => {
-    // const result = document.querySelector(".root");
-    // result.filterPokeType = $`{event.target.value}`;
-    const type = pokemonTypeMenu.value;
-    const mostrarTodos = filterPokeType(arrayPokemon, type);
- 
-    showData(mostrarTodos);
-});
+selectType.addEventListener("change", () =>{
 
+  var x = document.getElementById("filterType").selectedIndex;
+  var y = document.getElementById("filterType").options;
+  var condition = y[x].text;
+  //let condition= selectType.options[selectType.selectIndex].text;
+const pokemonType=window.filterPokeType(dataPokemon, condition)
+
+document.getElementById('hola').innerHTML += pokemonType;
+
+console.log(pokemonType)
+
+
+let showType = document.getElementById("root");
+const createCards2 = (data) => {
+let pokemonCard2 ='';
+
+  data.forEach((pokemon) => {
+    //CREAMOS LA TARJETA CON LOS ELEMENTOS DEL ARRAY
+    card = `
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <img src="${ pokemon.img }" /><br>
+    <b>Nº </b>${ pokemon.num }</b><br>
+    <b>${ pokemon.name }</b><br>
+    <b>Tipo </b><br> ${ pokemon.weaknesses }</b><br>
+    <b>Debilidades </b><br> ${ pokemon.weaknesses }<br>
+    <b>Huevos </b><br> ${pokemon.egg }</b><br><br>
+    </div>
+    </div>S
+    </div>
+    `;
+    pokemonCard2 += card
+  })
+  showType.innerHTML=pokemonCard2
+}
+//MOSTRAMOS LA DATA DESDE EL JSON
+createCards2(pokemonType)
+
+})
+
+
+
+
+
+
+/*// // OBTENIENDO VALOR DE TIPO SELECCIONADO POR USUARIO en dropdown
+// document.getElementById("filterType").addEventListener("change", () => {
+// let pokeType= document.getElementById("filterType");
+// let chosenType= pokeType.options[pokeType.selectedIndex].text;
+// console.log(chosenType);
+// })
+
+// let a = document.getElementById("filterType");
+//  a.addEventListener("change", () => {
+//   let pokeType= a.value;
+//     if (pokeType === "Fire"){
+//       window.filterPokeType(arrayPokemon);
+//       crearPlantilla="";
+//       crearPlantilla(arrayPokemon);
+//     }
+
+//   })*/
